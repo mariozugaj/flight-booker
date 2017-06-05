c6d72c2ab8dabc6f1808db7721d8fe9c02867f74
chanjman <mariozugaj@gmail.com>
2017-06-05 14:29:49 +0200

    Fix spelling mistake in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "FlightBooker_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  onfig.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'glacial-stream-23705.herokuapp.com' }
 
